ba0f7a9f44c1a32d2dccd5a5cdbad751b84ed0ce
99003553 <78540508+99003553@users.noreply.github.com>
2021-02-06 12:03:48 +0530

    Update multiplication.c

--- 4_Implementation/src/multiplication.c
+++ 4_Implementation/src/multiplication.c
@@ -4,7 +4,7 @@ int multiplication()
 {
     double j, k, mult;
     print("Two numbers:");
-    scanf("%1f %1f , &j, &k");
+    scanf("%1f %1f" , &j, &k);
     mult = j * k ;
     printf("mult = %.21f",mult);
 
