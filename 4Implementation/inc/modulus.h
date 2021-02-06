565c1a9e137b29bc7937899c561baf7843efcee1
Alen Reji Kokkad <69103817+stepin104484@users.noreply.github.com>
2021-02-06 14:55:03 +0530

    Create modulus.h

---
+++ 4Implementation/inc/modulus.h
@@ -0,0 +1,16 @@
+/**
+ * @file exponent.h
+ * @author Alen Reji Kokkad
+ * @brief Modulus function for calculator operation
+ * @version 0.1
+ * @date 2021-02-05
+ */
+
+#ifndef __MODULUS_H__
+#define __MODULUS_H__
+
+#include <stdio.h>
+
+float modulus(float num, float num1);
+
+#endif
