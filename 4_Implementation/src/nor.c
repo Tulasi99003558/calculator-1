a58e86e6926ad416a4a56de4431695ebd8435d0c
Alen Reji Kokkad <69103817+stepin104484@users.noreply.github.com>
2021-02-06 10:47:01 +0530

    Create nor.c

---
+++ 4_Implementation/src/nor.c
@@ -0,0 +1,12 @@
+#include<nor.h>
+int nor(int operand1, int operand2)
+{
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==0 && operand2==0)
+    return 1;
+    else
+    return 0;
+}
