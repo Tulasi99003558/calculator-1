68839c97842604e9d02c599c21f701b6acf2020c
99003553 <78540508+99003553@users.noreply.github.com>
2021-02-06 12:22:07 +0530

    Add files via upload

---
+++ inc/exponent.h
@@ -0,0 +1,16 @@
+/**
+ * @file exponent.h
+ * @author Alen Reji Kokkad
+ * @brief Exponential function for calculator operation
+ * @version 0.1
+ * @date 2021-02-05
+ */
+
+#ifndef __EXPONENT_H__
+#define __EXPONENT_H__
+
+#include <stdio.h>
+
+double exponent(float num);
+
+#endif
