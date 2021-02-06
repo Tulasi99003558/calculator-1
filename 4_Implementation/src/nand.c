3d0e4f9b5063fd75b8f24c8c611eca595cdfb7b8
Alen Reji Kokkad <69103817+stepin104484@users.noreply.github.com>
2021-02-06 10:46:58 +0530

    Create nand.c

---
+++ 4_Implementation/src/nand.c
@@ -0,0 +1,15 @@
+#include<nand.h>
+
+
+int nand(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==1 && operand2==1)
+    return 0;
+    else
+    return 1;
+}
